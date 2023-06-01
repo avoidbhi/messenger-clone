dc7d152c54dba5ddabbd01e8e8551904de214d17
Abhijeet Kanase <chillgoziecoder@outlook.com>
2023-06-01 17:24:55 +0530

    Commit Current changes

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,150 @@
+'use client'; //to not treat it as a server component but as a client component
+import Button from "@/app/components/Buttson";
+import Input from "@/app/components/inputs/input";
+import { useCallback, useState } from "react";
+import { 
+    FieldValues, 
+    SubmitHandler, 
+    useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub } from 'react-icons/bs';
+
+type Variant = 'LOGIN' | 'REGISTER';
+
+const AuthForm = () => {
+    const [variant, setVariant] = useState<Variant>('LOGIN');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const toggleVariant = useCallback( () => 
+    {
+    if (variant === 'LOGIN') 
+        {
+            setVariant('REGISTER');
+        } 
+    else 
+        {setVariant('LOGIN'); }
+    },[variant]);
+    
+//Submit-form
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name:'',
+            email:'',
+            password:''
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) =>
+     {
+           setIsLoading(true);
+
+           if(variant === 'REGISTER')
+           {
+            //AXIOS Register
+
+           }
+           if (variant === 'LOGIN')
+           {
+            //NextAuth social SignIn - using to build authentication of the app
+
+           }
+
+    }
+    const socialAction = (action: string) => {
+        <div>Auth Form2!</div>
+    }
+
+
+
+    return (
+        <div
+            className="
+            mt-8
+            sm:mx-auto
+            sm:w-full
+            sm:max-w-md
+            "
+        >    
+        <div
+        className="
+        bg-white
+        px-4
+        py-8
+        shadow
+        sm-rounded-lg
+        sm:px-10
+        "
+        >
+            <form
+            className="space-y-6"
+            onSubmit={handleSubmit(onSubmit)}
+            >
+                {variant === 'REGISTER' && (
+                <Input 
+                    id="email" 
+                    label="Email"
+                    register={register}
+                    errors={errors}
+                />
+                )}
+                <Input 
+                    id="email" 
+                    label="Email Address"
+                    type="email"
+                    register={register}
+                    errors={errors}
+                />
+                <Input 
+                    id="password" 
+                    label="Password"
+                    type="password"
+                    register={register}
+                    errors={errors}
+                />
+                <div>
+                    <Button
+                       disabled={isLoading}
+                       fullWidth
+                       type="submit"
+                    >
+                        {variant === 'LOGIN' ? 'Sign In' : 'Register'}
+                    </Button>
+                </div>
+            </form>
+
+                    <div className="mt-6">
+                            <div className="relative">
+                                <div 
+                                className="
+                                    absolute 
+                                    inset-0 
+                                    flex 
+                                    items-center
+                                "
+                                >
+                                <div className="w-full border-t border-gray-300" />
+                                </div>
+                                <div className="relative flex justify-center text-sm">
+                                <span className="bg-white px-2 text-gray-500">
+                                    Or continue with
+                                </span>
+                                </div>
+                            </div>
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialButton 
+                            icon={BsGithub}
+                        />
+                    </div>
+                    </div>
+                </div>
+            </div>
+    );
+}
+
+export default AuthForm;
